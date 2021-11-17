78f30413c1203755ad50a617f6a2cdb364decfd3
Martin Staadecker <machstg@gmail.com>
2021-11-17 10:02:32 -0500

    Initial commit with working local extension

---
+++ remove_notifications.js
@@ -0,0 +1,20 @@
+// Function that corrects the title
+function get_no_notification_title(title) {
+  return title.replace(/\([^()]*\)/, "") // Removes anything between parentheses
+}
+
+// Set the title
+document.title = get_no_notification_title(document.title)
+
+// Create a listener so that if the title changes, it is also corrected
+function onTitleChange(_) {
+  const noNotificationTitle = get_no_notification_title(document.title);
+  if (document.title !== noNotificationTitle)
+    document.title = noNotificationTitle
+}
+
+// Register listener
+new MutationObserver(onTitleChange).observe(
+  document.querySelector('title'),
+  {subtree: true, characterData: true, childList: true}
+);
